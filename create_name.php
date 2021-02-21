6c4f7f5c54315d41e4e9c7425176550659918cdc
Domm <32127496+domm-it@users.noreply.github.com>
2021-02-21 16:48:40 +0100

    Add files via upload
    Add Comments

--- create_name.php
+++ create_name.php
@@ -1,28 +1,45 @@
 <?php
     function create_name($gender="m",$letter=false){
-        $vocals = array("a","e","i","o","u");
+
+        /*=======================================================
+        # LETTERS
+        =======================================================*/
+        $vowels = array("a","e","i","o","u");
         $consonants = array("b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z");
         
+        /*=======================================================
+        # GET LETTERS (ONLY ON RECURSION)
+        =======================================================*/
         if($letter=="v"){
-            return $vocals[rand(0,count($vocals)-1)];
+            return $vowels[rand(0,count($vowels)-1)];
         }elseif($letter=="c"){
             return $consonants[rand(0,count($consonants)-1)];
         }
+
+        /*=======================================================
+        # GET DIMENSION OF NAME RANDOM
+        =======================================================*/
         $length = rand(3,8);
         $cons_tot = 2;
-        $voc_tot = 1;
-
+        $vow_tot = 1;
         $con=0;
-        $voc=0;
+        $vow=0;
         $last = "";
         $name = "";
+
+        /*=======================================================
+        # ITERATION TO CREATE WORD
+        =======================================================*/
         for($i=0; $i<$length-1; $i++){
             if($i==0){
+                /*=======================================================
+                # RANDOM VOWELS OR CONSONANTS (ON RECURSION)
+                =======================================================*/
                 if(rand(0,1000)%2==0){
                     $letter = create_name($gender,1);
                     $last = $letter;
                     $name .= $letter;
-                    $voc+=1;
+                    $vow+=1;
                 }else{
                     $letter = create_name($gender,2);
                     $last = $letter;
@@ -31,13 +48,16 @@
                 }
             }
 
-            if($voc!=$voc_tot){
+            /*=======================================================
+            # CONTROL TOO MUCH VOWELS OR CONSONANTS
+            =======================================================*/
+            if($vow!=$vow_tot){
                 do{
                     $letter = create_name($gender,1);
                 }while($letter==$last);
                 $last = $letter;
                 $name.=$letter;
-                $voc+=1;
+                $vow+=1;
                 $con=0;
             }elseif($con!=$cons_tot){
                 do{
@@ -46,14 +66,21 @@
                 $last = $letter;
                 $name.=$letter;
                 $con+=1;
-                $voc=0;
+                $vow=0;
             }
 
+            /*=======================================================
+            # INSERT LAST LETTER "A" ON FEMALE NAMES - itaFix
+            =======================================================*/
             if($gender=="f" && $i==$length-2){ 
                 if(substr($name, 0, strlen($name)-2) != "a"){
                     $name = substr($name, 0, strlen($name)-1)."a";
                 }
             }
         }
+
+        /*=======================================================
+        # PRINT NAME
+        =======================================================*/
         return ucfirst($name);
     }
